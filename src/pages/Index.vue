0846ffd196dfc1d38ff80693674d25bb71979f4d
funkmatch <daviddedobbeleer@davids-mbp.home>
2020-06-08 23:10:50 +0200

    changed p for div in video section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -113,7 +113,142 @@
       </ul>
     </section>
 
-   
+    <!-- Youtube embbeder section -->
+    <section>
+      <!-- h2 -->
+      <h2
+        class="w-full px-2 mt-10 mb-2 text-4xl font-bold leading-tight text-center text-gray-800"
+      >
+        Visites sur chantier...
+      </h2>
+
+      <!-- Underline title -->
+      <div class="w-full mb-4">
+        <div class="h-1 mx-auto gradient w-64 opacity-25 rounded-t"></div>
+      </div>
+
+      <!-- Rounded wrapper -->
+      <div
+        class="flex m-auto p-4 py-10 max-w-screen-xl sm:w-2/3 md:w-full md:p-12"
+      >
+        <!--  bg-pink-500  -->
+        <div
+          class="flex flex-col mx-auto py-4 w-full rounded-lg bg-white shadow-lg md:flex-row md:pt-4"
+        >
+          <!-- bg-teal-500 -->
+
+          <!-- Subsection image/video/buttF -->
+          <div class="md:w-7/12 pt-2 px-4 md:px-0 md:pl-4">
+            <!-- bg-blue-500 -->
+            <div class="relative mx-auto h-48 maxmob:w-2/3 middle:w-full z-30">
+              <div class="absolute w-full h-auto pin-c z-20">
+                <div class="md:mt-36 lg:mt-48" @click="playVideo()">
+                  <SvgPlayButton
+                    class="mx-auto w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 z-30"
+                  />
+                </div>
+              </div>
+
+              <g-image
+                class="absolute w-full pin-c border rounded-lg shadow-lg z-10"
+                :class="'video-position'"
+                :src="$page.data.acf.icImage"
+              />
+
+              <!-- <div class="relative" :class="'video-position'"> -->
+
+              <!-- </div> -->
+            </div>
+          </div>
+
+          <VideoModal v-if="showModal" @close="showModal = false">    
+            <div slot="body">
+              <iframe                
+                class="w-full h-56 maxmob:h-64 middle:h-80 medium:h-96 large:h-112 xlarge:h-160 rounded-lg"
+                :class="videoOnTopClass"
+                src="https://www.youtube.com/embed/VZzyFo2V6NI?autoplay=1"
+              ></iframe>
+            </div>
+          </VideoModal>
+
+          <!-- Subsection text -->
+          <div class="flex mx-4 mt-6 mb-4 md:w-5/12">
+            <!-- bg-purple-500 -->
+            <div class="h-auto md:h-96 lg:px-8">
+              <div class="text-gray-700 text-lg xl:text-xl">
+                <div class="text-justify" v-html="$page.data.acf.icContent"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- mute the vid : https://stackoverflow.com/questions/41185416/how-can-i-mute-an-iframe-with-an-embedded-youtube-video-in-html -->
+    </section>
+
+    <!-- <BannerQualifLong /> -->
+
+    <!--Qualifications section -->
+    <section>
+      <!-- h2 -->
+      <h2
+        class="w-full px-2 mt-10 mb-2 text-4xl font-bold leading-tight text-center text-gray-800"
+      >
+        PSR, triplement certifiée
+      </h2>
+
+      <!-- Underline title -->
+      <div class="w-full mb-4">
+        <div class="h-1 mx-auto gradient w-64 opacity-25 rounded-t"></div>
+      </div>
+
+      <div class="flex flex-col m-auto p-4 max-w-screen-xl md:p-12 md:flex-row">
+        <div class="flex pl-4 pr-4 md:flex-col md:w-1/3">
+          <div class="flex-1"></div>
+          <div class="flex lg:rounded lg:border lg:p-4 lg:-mt-6">
+            <div class="text-gray-700 text-lg xl:text-xl">
+              <p
+                class="text-justify"
+                v-html="
+                  $page.data3.acf.repeaterCard[0].repeaterParagraph[0]
+                    .htmlParagraph
+                "
+              ></p>
+            </div>
+          </div>
+          <div class="flex-1"></div>
+        </div>
+        <div class="py-6 md:px-0 md:w-2/3">
+          <div class="flex justify-center mb-8">
+            <ul class="flex">
+              <li
+                v-for="(image, index) in $page.data2.acf.rGallery"
+                :key="index"
+                class="flex-1"
+              >
+                <div class="flex px-2">
+                  <div class="flex-1"></div>
+                  <g-image
+                    class="h-20 sm:h-24 lg:h-32 border rounded flex-1"
+                    :src="image.repeaterImages"
+                  />
+                  <div class="flex-1"></div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- <div class="mx-8 mb-8 ssm:mx-12 mid:mx-20 md:mb-12 lg:px-40">
+        <div class="text-gray-700 text-base xl:text-xl">
+          <p
+            class="text-justify"
+            v-html="$page.data2.acf.riRepeaterContent[0].content"
+          ></p>
+        </div>
+      </div> -->
+      </div>
+    </section>
 
     <!--Pre-footer slots section -->
     <SlotBottomSection>
