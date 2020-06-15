3b848687cc46c06082d6199e65bc6cef4229be4b
funkmatch <daviddedobbeleer@davids-mbp.home>
2020-06-15 20:14:33 +0200

    Buffer.from(username + ':' + password, 'binary').toString('base64');

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,17 @@
 // File: /src/services/api.js
 
 // Build header with Basic Auth
+const btoaCustom = (str) => {
+	Buffer.from(str, 'binary').toString('base64');
+};
 const username = 'Gridsome';
 const password = '4pVX KPTy lfij 2KVH ByWQ prcG';
 const apiUrl =
 	'https://cors-anywhere-cq.herokuapp.com/https://psr2222.creativityquarks.com/';
 const headers = { 'Content-Type': 'application/json' };
-headers.Authorization = 'Basic ' + btoa(username + ':' + password);
+headers.Authorization = 'Basic ' + Buffer.from(username + ':' + password, 'binary').toString('base64');
+// headers.Authorization = 'Basic ' + btoaCustom(username + ':' + password);
+// headers.Authorization = 'Basic ' + btoa(username + ':' + password);
 
 export const sendReview = (title, fields) => {
 	console.log('From send review, sent this object to backend:');
@@ -17,9 +22,9 @@ export const sendReview = (title, fields) => {
 		method: 'POST',
 		headers,
 		body: JSON.stringify({ title, fields }),
-  })
-    
-  // .then((response) => {
+	});
+
+	// .then((response) => {
 	// 	console.log(response.json());
 	// 	if (response.status === 201) {
 	// 		$router.push({ path: '/success/' });
